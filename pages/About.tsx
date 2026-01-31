4aecea8f55e922cd913dd2a7120bcbd527f2a37a
HamzaDeveloperr <alihamza400002@gmail.com>
2026-01-31 08:51:43 +0500

    Update About.tsx

--- pages/About.tsx
+++ pages/About.tsx
@@ -19,7 +19,7 @@ const About: React.FC = () => {
           <div className="lg:col-span-4 space-y-8 lg:sticky lg:top-32 reveal">
             <div className="relative rounded-[40px] overflow-hidden border-8 border-white shadow-2xl">
 <img 
-  src={hamzaAbout} 
+  src="./Images/hamza_about.jpeg" 
   alt={CONFIG.name} 
   className="w-full h-auto object-cover" 
 />            </div>
